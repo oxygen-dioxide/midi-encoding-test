4d64187f5616959ce7af543b329590a44f799194
oxygen-dioxide <54425948+oxygen-dioxide@users.noreply.github.com>
2022-08-21 13:38:28 +0800

    zh-UTF8 and zh-GBK

---
+++ generate.py
@@ -0,0 +1,20 @@
+import yaml
+import mido
+#将ustx转为midi，使用指定的字符编码
+def convert(inputfile:str,outputfile:str,encoding:str):
+    ustx=yaml.load(open(inputfile,encoding="utf8"),Loader=yaml.FullLoader)
+    midifile = mido.MidiFile(charset=encoding)
+    for ustxpart in ustx["voice_parts"]:
+        miditrack=mido.MidiTrack()
+        time=0
+        for ustxnote in ustxpart["notes"]:
+            miditrack.append(mido.MetaMessage('lyrics',text=ustxnote["lyric"],time=(int(ustxnote["position"])-time)))#歌词
+            miditrack.append(mido.Message('note_on', note=int(ustxnote["tone"]),velocity=64,time=0))
+            miditrack.append(mido.Message('note_off',note=int(ustxnote["tone"]),velocity=64,time=int(ustxnote["duration"])))
+            time=int(ustxnote["position"])+int(ustxnote["duration"])
+        miditrack.append(mido.MetaMessage('end_of_track'))
+        midifile.tracks.append(miditrack)
+    midifile.save((outputfile))
+
+convert("zh.ustx","zh-UTF8.mid","UTF-8")
+convert("zh.ustx","zh-GBK.mid","GBK")
